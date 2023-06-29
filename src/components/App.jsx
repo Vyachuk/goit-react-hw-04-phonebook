b2eb378db6436201146ee8a64be4ae32453e46b3
Vyachuk <Slavkoka045@gmail.com>
2023-06-29 16:45:06 +0300

    test 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,7 +15,7 @@ export class App extends Component {
       // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
-  };
+  }; //asd
   componentDidMount() {
     const contactStorage = load('contactList');
     if (contactStorage) {
